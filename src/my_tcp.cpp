ed020a9762d8510e3fd5dcdc2ade3804624b18fa
Lilwaysk <139118198+Lilwaysk@users.noreply.github.com>
2026-09-18 09:03:05 +0800

    实现 fork 并发 TCP 服务器:接受连接后用子进程处理并把输入转大写回显

--- src/my_tcp.cpp
+++ src/my_tcp.cpp
@@ -12,18 +12,65 @@
 #include <unistd.h>
 #include <iostream>
 
-// #include "wrap.h"
+#include "wrap.h"
 #define SRV_PORT 9999
 
 using namespace std;
 
 int main()
 {
-    int lfd;
-    struct sockaddr_in srv_addr;
+    int lfd, cfd, ret;
+    pid_t pid;
+    struct sockaddr_in srv_addr, clt_addr;
+    char buf[BUFSIZ];
+    int i;
+    socklen_t clt_addr_len;
+
+    // 将地址结构清零
+    bzero(&srv_addr, sizeof(srv_addr));
+
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_port = htons(SRV_PORT);
     srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+    lfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+    Bind(lfd, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
+
+    Listen(lfd, 128);
+
+    clt_addr_len = sizeof(clt_addr);
+
+    while(1) {
+        // 连接成功就返回一个新的cfd，对应一个新客户端
+        cfd = Accept(lfd, (struct sockaddr *)&clt_addr , &clt_addr_len);
+
+        // fork返回进程id
+        pid = fork();
+        if (pid < 0)
+            perr_exit("fork error");
+        else if (pid == 0) {
+            close(lfd);
+            break;
+        } else {
+            close(cfd);
+            continue;
+        }
+
+    }
+
+    // 处理子进程
+    if(pid == 0) {
+        ret = read(cfd, buf, sizeof(buf));
+        if(ret == 0) {
+            close(cfd);
+            exit(1);
+        }
+
+        for(i = 0; i < ret; ++i)
+            buf[i] = toupper(buf[i]);
+        write(cfd, buf, ret);
+        write(STDOUT_FILENO, buf, ret);
+    }
     return 0;
 }
